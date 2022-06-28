4d3acb6ad1c3dd3b86b816d3251cc90fb057fd45
Adam Topczewski <gnomowisko@gmail.com>
2022-06-28 17:58:19 +0200

    added dotenv + editorial changes

--- index.js
+++ index.js
@@ -4,36 +4,25 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const { response } = require("express");
 
+require('dotenv').config()
+console.log(process.env)
 const app = express();
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
-const url = "https://r.pl/europa";
-// axios(url).then((res) => {
-//     const html_data = res.data;
-//     const $ = cheerio.load(html_data);
-//     // console.log($('.lista-kierunkow__row'));
-//     const kierunkiDiv = $('.lista-kierunkow__row');
-// })
+const url1 = process.env.URL1;
 
-function getDataFromPage(url) {
+function scrapeWebsiteDestinations(url) {
     axios(url).then((res) => {
         const html_data = res.data;
         const $ = cheerio.load(html_data);
-        // console.log($('.lista-kierunkow__row'));
-        const kierunkiDiv = $('.col-lg-6.col-md-6.col-sm-6.col-xs-12.kraj > div > a ~ a');
-        const formatedName = $('.col-lg-6.col-md-6.col-sm-6.col-xs-12.kraj > div > a.link');
+        const destinationElements = $('.col-lg-6.col-md-6.col-sm-6.col-xs-12.kraj > div > a ~ a');
+        const destinationHrefs = $('.col-lg-6.col-md-6.col-sm-6.col-xs-12.kraj > div > a.link');
 
-        for(let i = 0; i< kierunkiDiv.length; i++) {
-            console.log(kierunkiDiv.eq(i).text())
-            console.log(formatedName.eq(i).attr('href'))
+        for(let i = 0; i< destinationElements.length; i++) {
+            // console.log(destinationElements.eq(i).text())
+            // console.log(destinationHrefs.eq(i).attr('href'))
         }
-        // kierunkiDiv.each( function(i, el) {
-        //     console.log($(this).text())
-        // })
-        // formatedName.each( function(i, el) {
-        //     console.log($(this).attr('href'))
-        // })
     })
 }
 
-getDataFromPage(url);
+scrapeWebsiteDestinations(url1);
