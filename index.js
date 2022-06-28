e13d8802a847beb30671397a19105f9ae3d2e385
Adam Topczewski <gnomowisko@gmail.com>
2022-06-28 17:42:46 +0200

    added scraping href and text for matching puropses

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ const { response } = require("express");
 const app = express();
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
-const url = "https://r.pl/";
-axios(url).then((res) => {
-    const html_data = res.data;
-    const $ = cheerio.load(html_data);
-    // console.log($('.lista-kierunkow__row'));
-    const kierunkiDiv = $('.lista-kierunkow__row');
-})
+const url = "https://r.pl/europa";
+// axios(url).then((res) => {
+//     const html_data = res.data;
+//     const $ = cheerio.load(html_data);
+//     // console.log($('.lista-kierunkow__row'));
+//     const kierunkiDiv = $('.lista-kierunkow__row');
+// })
+
+function getDataFromPage(url) {
+    axios(url).then((res) => {
+        const html_data = res.data;
+        const $ = cheerio.load(html_data);
+        // console.log($('.lista-kierunkow__row'));
+        const kierunkiDiv = $('.col-lg-6.col-md-6.col-sm-6.col-xs-12.kraj > div > a ~ a');
+        const formatedName = $('.col-lg-6.col-md-6.col-sm-6.col-xs-12.kraj > div > a.link');
+
+        for(let i = 0; i< kierunkiDiv.length; i++) {
+            console.log(kierunkiDiv.eq(i).text())
+            console.log(formatedName.eq(i).attr('href'))
+        }
+        // kierunkiDiv.each( function(i, el) {
+        //     console.log($(this).text())
+        // })
+        // formatedName.each( function(i, el) {
+        //     console.log($(this).attr('href'))
+        // })
+    })
+}
+
+getDataFromPage(url);
